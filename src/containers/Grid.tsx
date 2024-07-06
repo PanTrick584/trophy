b24d04a231224891426e115fd78aa7818c8433b3
Patryk <chodacki.pc@gmail.com>
2024-07-06 20:08:24 +0200

    fix for build

--- src/containers/Grid.tsx
+++ src/containers/Grid.tsx
@@ -3,14 +3,12 @@ import { ReactNode } from "react";
 interface GridProps {
     children: ReactNode;
     sectionNum: string;
-    rowNumber: number
 }
 
-const Grid = ({children, sectionNum, rowNumber}: GridProps) => {
-  const style = {
-    gridTemplateRows: rowNumber ? `repeat(${rowNumber}, auto)` : `repeat(3, auto)`
-  }
-  return <div className={`section section-${sectionNum} section-grid section-grid-${sectionNum}`}>{children}</div>
+const Grid = ({children, sectionNum}: GridProps) => {
+    return (
+        <div className={`section section-${sectionNum} section-grid section-grid-${sectionNum}`}>{children}</div>
+    )
 }
 
 export default Grid;
